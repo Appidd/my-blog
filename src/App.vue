7292ee50fc083db06c5f5fa742927bb3395c085d
Appidd <1932844332@qq.com>
2024-06-24 17:22:35 +0800

    ceshi

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite + ts" />
+  <HelloWorld msg="Hello Vue 3 + Vite + typescript!" />
 </template>
 
 <script setup>
