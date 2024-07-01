77fd82dab937e217b4c5a7f1edd58af812cc4708
Appidd <1932844332@qq.com>
2024-07-01 10:41:40 +0800

    feat:车是

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite + typescript!" />
+  <HelloWorld msg="Hello Vue 3 + Vite + typescript! hhhhhhhh" />
 </template>
 
 <script setup>
